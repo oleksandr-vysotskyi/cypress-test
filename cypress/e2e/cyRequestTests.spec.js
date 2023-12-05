ee99410645fe67a71260ef258af33b6075b28b2a
Vysotskyi, Oleksandr <oleksandr.v.vysotskyi@gmail.com>
2023-12-05 22:16:55 +0200

    homework_lesson_16

---
+++ cypress/e2e/cyRequestTests.spec.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress"/>
+
+describe('api tests', () => {
+  
+    it('GET check', () => {
+      cy.request('GET', 'https://jsonplaceholder.typicode.com/posts')
+        .then(resp => {
+          expect(resp.status).to.eq(200);
+        });
+    });
+
+    it('POST check', () => {
+        cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', {
+          title: 'name',
+          body: 'body',
+          userId: 2,
+        }).then(resp => {
+          expect(resp.status).to.eq(201);
+          expect(resp.body).to.have.property('userId', 2);
+        });
+      });
+  
+    it('PUT check', () => {
+      cy.request('PUT', 'https://jsonplaceholder.typicode.com/posts/1', {
+        title: 'test'
+      }).then(resp => {
+        expect(resp.status).to.eq(200);
+      });
+    });
+  
+    it('PATCH check', () => {
+      cy.request('PATCH', 'https://jsonplaceholder.typicode.com/posts/1', {
+         title: 'new name'
+        }).then(resp => {
+          expect(resp.status).to.eq(200);
+          expect(resp.body).to.have.property('title', 'new name');
+        });
+      });
+      
+  
+    it('DELETE check', () => {
+      cy.request('DELETE','https://jsonplaceholder.typicode.com/posts/101').then(resp => {
+          expect(resp.status).to.eq(200);
+          expect(resp).to.have.property('duration');
+        });
+    });
+  });
+  
